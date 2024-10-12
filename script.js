67446b9584f0c8941a46f24b43e9186529966d89
Srijan Sinha <103751862+Srijandevhub@users.noreply.github.com>
2024-10-12 07:27:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
 //complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this.species = species;
+  }
 
-class Dog extends Animal {}
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  set species(value) {
+    this._species = value;
+  }
+
+  makeSound() {
+    console.log(`The ${this.species} makes a sound.`);
+  }
+}
+
+class Cat extends Animal {
+  constructor(species) {
+    super(species);
+  }
+
+  purr() {
+    console.log("Purr");
+  }
+}
+
+class Dog extends Animal {
+  constructor(species)   
+ {
+    super(species);
+  }
+
+  bark() {
+    console.log("Woof");   
+
+  }
+}
+
 
 // Do not change the code below this line
 window.Animal = Animal;
